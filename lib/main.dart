9e07cec265a297f24db0f326ebe814fc2c4c2626
Joao_Ferreira <jpjesusf08@gmail.com>
2022-06-06 20:57:51 +0100

    Multi widgets on body

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,41 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My Fisrt App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            ElevatedButton(
+              // Like Raised Button
+              child: Text('Answer 1'),
+              onPressed: () {
+                print('Answer 1 chosen!');
+              },
+            ),
+            ElevatedButton(
+              // Like Raised Button
+              child: Text('Answer 2'),
+              onPressed: () {
+                print('Answer 2 chosen!');
+              },
+            ),
+            ElevatedButton(
+              // Like Raised Button
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
