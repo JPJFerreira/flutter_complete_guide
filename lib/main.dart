0cd79a9e82bc91aa6d681160d49a4929d202c87b
Joao_Ferreira <jpjesusf08@gmail.com>
2022-06-09 23:14:15 +0100

    If statement

--- lib/main.dart
+++ lib/main.dart
@@ -15,44 +15,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': ['Red', 'Green', 'Blue']
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': ['Dog', 'Cat', 'Rabbit', 'Lion']
+    },
+    {
+      'questionText': 'What\'s your favorite instructor?',
+      'answers': ['Max', 'João', 'Max']
+    }
+  ];
+
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    // if (_questionIndex < questions.length - 1) {
     setState(() {
-      _questionIndex = _questionIndex + 1;
+      _questionIndex++;
     });
+    // }
     print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s your favorite color?',
-        'answers': ['Red', 'Green', 'Blue']
-      },
-      {
-        'questionText': 'What\'s your favorite animal?',
-        'answers': ['Dog', 'Cat', 'Rabbit']
-      },
-      {
-        'questionText': 'What\'s your favorite instructor?',
-        'answers': ['Max', 'João', 'Max']
-      }
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My Fisrt App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-            Answer(_answerQuestion),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: [
+                  Question(questions[_questionIndex]['questionText'] as String),
+                  ...(questions[_questionIndex]['answers'] as List<String>)
+                      .map((answer) {
+                    return Answer(_answerQuestion, answer);
+                  }).toList()
+                ],
+              )
+            : Center(
+                child: Text('You\'ve reached the end of the quiz!'),
+              ),
       ),
     );
   }
